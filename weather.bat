2ec5234fc33c0aa9064e679e149fa78c9107e902
dorfe <Fedor.Gavrilov@gmail.com>
2016-03-29 13:30:51 +0300

    .

--- weather.bat
+++ weather.bat
@@ -34,6 +34,6 @@ for (var i = 20 * timeout; x.readyState != 4 && i >= 0; i--) {
 var HTMLDoc = new ActiveXObject("HTMLFile");
 HTMLDoc.write(x.responseText);
 
-var res = HTMLDoc.getElementsByClassName("today-forecast")[0].innerText + ", " +HTMLDoc.getElementsByClassName("temperature-wrapper")[0].innerText.replace("\n", "").replace("\r", "").replace("\n", "").replace("\r", "").replace("\u2009\u02DA", " \u00B0").replace("м/с", "метра в секунду");
+var res = HTMLDoc.getElementsByClassName("today-forecast")[0].innerText + ", " +HTMLDoc.getElementsByClassName("temperature-wrapper")[0].innerText.replace("\n", "").replace("\r", "").replace("\n", "").replace("\r", "").replace("\u2009\u02DA", " \u00B0");
 
-WSH.Echo(res);
+WSH.Echo(res.replace("/с", " в секунду"));
