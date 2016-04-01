1622a1bb9be1a1c27ec4297bacdc2852057d4b8f
dorfe <Fedor.Gavrilov@gmail.com>
2016-04-01 22:38:53 +0300

    .

--- say_time_ru.bat
+++ say_time_ru.bat
@@ -1,10 +1,12 @@
-rem @echo off
+@if (@a==@b) @end /*
+
+:: batch script portion
+
+@echo off
+
+setlocal
 
 chcp 1251 >nul
-
-rem for /F %%i in ('time /t') do speak.exe %1 "%%i"
-
-rem for /F "delims=" %%i in ('time ^<nul ^| cmd /q /v:on /c "set/p .=&echo(!.!"') do say_ru.bat "%%i"
 
 rem set HH=%TIME: =0%
 rem set HH=%HH:~0,2%
@@ -14,4 +16,20 @@ if "%HH:~,1%"=="0" set HH=%HH:~1,2%
 set MI=%time:~3,2%
 if "%MI:~,1%"=="0" set MI=%MI:~1,2%
 
-say_ru.bat "Текущее время: <rate absspeed="3"><emph>%HH%<t1><hr></emph> часов <emph>%MI%<t1><min></emph> минут</rate>"
+say_ru.bat "Текущее время: <rate absspeed="3"><emph>%HH%<t1><hr></emph> часов <emph>%MI%<t1><min> минут</emph></rate>"
+
+for /f "delims=" %%A in ('cscript /nologo /e:jscript "%~f0" "%HH%" "%MI%"') do echo.%%A
+
+goto :EOF
+
+:: JScript portion */
+
+function declOfNum(number, titles)  
+{  
+    cases = [2, 0, 1, 1, 1, 2];  
+    return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];  
+}
+
+var res = "Текущее время: <rate absspeed="3"><emph>" + WSH.Arguments(0) + "<t1><hr></emph> " + declOfNum(WSH.Arguments(0),['час','часа','часов']) + "<emph>" + WSH.Arguments(1) + "<t1><min> " + declOfNum(WSH.Arguments(1),['минута','минуты','минут']) + "</emph></rate>";
+
+WSH.Echo(res);
