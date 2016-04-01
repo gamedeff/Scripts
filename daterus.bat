a387de7efa30af930c2213191d2d41aae8f5ec4f
Fedor Gavrilov <Fedor.Gavrilov@gmail.com>
2016-04-01 09:58:16 +0300

    .

--- daterus.bat
+++ daterus.bat
@@ -1,8 +1,12 @@
-@echo off
-:: format: dd.mm.yyyy
-:: кодировка кирилица DOS (866)
+@if (@a==@b) @end /*
 
-rem echo %date%
+:: batch script portion
+
+@echo off
+
+chcp 65001 >nul
+
+setlocal
 
 set /a y=%date:~6,4%&set /a m=1%date:~3,2%-100&set /a d=1%date:~0,2%-100
 set /a i=(%y%-1901)*365 + (%y%-1901)/4 + %d% + (!(%y% %% 4))*(!((%m%-3)^&16))
@@ -10,6 +14,29 @@ set /a i=(%i%+(%m%-1)*30+2*(!((%m%-7)^&16))-1+((65611044^>^>(2*%m%))^&3))%%7+1
 ::  (igor_andreev)
 for /f "tokens=%i% delims=/" %%a in ('
 echo/Понедельник/Вторник/Среда/Четверг/Пятница/Суббота/Воскресенье') do set "w=%%a"
-for /f "tokens=%m% delims=/" %%a in ('
-ECHO/Января/Февраля/Марта/Апреля/Мая/Июня/Июля/Августа/Сентября/Октября/Ноября/Декабря/') do set "mmm=%%a"
-echo Сегодня %w%, %d% %mmm% %y% года
+
+for /f "delims=" %%A in ('cscript /nologo /e:jscript "%~f0" "%date%"') do echo Сегодня %w%, %%A
+
+goto :EOF
+
+:: JScript portion */
+
+function die(txt) {
+    WSH.StdErr.WriteLine(txt.split(/\r?\n/).join(' '));
+    WSH.Quit(1);
+}
+
+var x=new ActiveXObject("MSXML2.ServerXMLHTTP");
+x.open("POST", "http://udarenieru.ru/desktop_1/ajax/datum.php", false);
+x.setRequestHeader('User-Agent','XMLHTTP/1.0');
+x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+x.send('datum=' + WSH.Arguments(0) + '&decl=0');
+var timeout = 60;
+for (var i = 20 * timeout; x.readyState != 4 && i >= 0; i--) {
+    if (!i) die("Timeout error.");
+    WSH.Sleep(50)
+};
+
+//if (!x.responseXML.hasChildNodes) die(x.responseText);
+
+WSH.Echo(eval("unescape('" + x.responseText.substring(x.responseText.indexOf('"c":"<span>') + 11).replace('<\\/span>"}', "") + "');"));
