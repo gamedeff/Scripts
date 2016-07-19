45cb0132f5785f0a89f469b9e122f892fb31b11a
Fedor Gavrilov <Fedor.Gavrilov@gmail.com>
2016-07-19 16:46:20 +0300

    .

--- movensayit.bat
+++ movensayit.bat
@@ -1,4 +1,11 @@
 @echo off
 
-nircmd.exe setcursor 1080 5
+rem nircmd.exe setcursor 1910 0
+rem nircmd.exe wait 1000
+rem nircmd.exe movecursor 10 0
+rem nircmd.exe wait 1000
+rem nircmd.exe movecursor 0 10
+
+nircmd.exe sendkeypress lwin+c
+
 sayit.bat
