38232aa8ca906050a01a72209904429601b91d6e
dorfe <Fedor.Gavrilov@gmail.com>
2016-07-21 03:08:50 +0300

    .

--- movensayit.bat
+++ movensayit.bat
@@ -1,5 +1,7 @@
 @echo off
 
+nircmd.exe setsysvolume 6000
+
 rem nircmd.exe setcursor 1910 0
 rem nircmd.exe wait 1000
 rem nircmd.exe movecursor 10 0
@@ -10,3 +12,5 @@ nircmd.exe sendkeypress lwin+c
 
 call sayit1.bat
 call sayit2.bat
+
+nircmd.exe setsysvolume 0
